91d71ad2355eb48164436437143b434c62e2e484
Alo Oluwapelumi David <pelumidavid123456@gmail.com>
2022-09-09 16:07:54 +0100

    commit

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"Book_Store_Management_System/pkg/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,34 @@
 package controllers
+
+import (
+	"Book_Store_Management_System/pkg/models"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("content-type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("content-type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
